77dac91bca082e05098a13103456109dfd44e9bf
arturoszulc@gmail.com <MIlmx*Qx.ZK~Xt#KP.s&>
2022-02-08 19:45:11 +0100

    Finished creating animating screen change

--- lib/view_layer/wrapper.dart
+++ lib/view_layer/wrapper.dart
@@ -1,15 +1,10 @@
-import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sepapka/model_layer/models/logged_user.dart';
+import 'package:sepapka/utils/consts/nav.dart';
 import 'package:sepapka/view_layer/authenticate/reset_password_screen.dart';
 import 'package:sepapka/view_layer/custom_widgets/app_loading.dart';
-import 'package:sepapka/viewmodel_layer/manager.dart';
 import 'package:sepapka/viewmodel_layer/nav_manager.dart';
-import 'package:sepapka/utils/consts/nav.dart';
 
-
-import 'authenticate/authenticate.dart';
 import 'authenticate/sing_in_screen.dart';
 import 'menu/menu_main_screen.dart';
 
@@ -21,35 +16,42 @@ class Wrapper extends StatelessWidget {
     debugPrint('*** Wrapper built ***');
 
     return Selector<NavManager, Screen>(
-          selector: (_, navManager) => navManager.currentScreen,
-        builder: (_, currentScreen, __) => AnimatedSwitcher(
-          duration: const Duration(milliseconds: 500),
-          transitionBuilder: (
-          Widget child,
-              Animation<double> animation) => ScaleTransition(child: child, scale: animation),
-          child: getWidget(currentScreen),
+      selector: (_, navManager) => navManager.currentScreen,
+      builder: (_, currentScreen, __) => AnimatedSwitcher(
+        duration: const Duration(milliseconds: 200),
+          switchOutCurve: const Threshold(0),
+        // switchInCurve: Curves.easeIn,
+        transitionBuilder: (Widget child, Animation<double> animation) => SlideTransition(
+          child: child,
+          position: Tween<Offset>(
+            begin: const Offset(1, 0),
+            end: Offset.zero,
+          ).animate(animation),
+
+          //animation.drive(Tween(begin: const Offset(0.0, 1.0), end: Offset.zero).chain(CurveTween(curve: Curves.ease))),
         ),
+        child: getWidget(currentScreen),
+      ),
 
-        // {
-        //     switch (currentScreen) {
-        //
-        //       case Screen.loading:
-        //         return const LoadingScreen();
-        //         break;
-        //       case Screen.signIn:
-        //         return SignInScreen();
-        //         break;
-        //       case Screen.menu:
-        //         return MenuScreen();
-        //         break;
-        //
-        //       case Screen.resetPassword:
-        //         return ResetPasswordScreen();
-        //         break;
-        //     }
-        // },
-
-      );
+      // {
+      //     switch (currentScreen) {
+      //
+      //       case Screen.loading:
+      //         return const LoadingScreen();
+      //         break;
+      //       case Screen.signIn:
+      //         return SignInScreen();
+      //         break;
+      //       case Screen.menu:
+      //         return MenuScreen();
+      //         break;
+      //
+      //       case Screen.resetPassword:
+      //         return ResetPasswordScreen();
+      //         break;
+      //     }
+      // },
+    );
 
     // return Selector<Manager, bool>(
     //   selector: (_, manager) => manager.loading,
@@ -60,12 +62,10 @@ class Wrapper extends StatelessWidget {
     //         loggedUser != null ? Menu() : Authenticate(),
     //   ),
     // );
-
   }
 
   Widget getWidget(Screen currentScreen) {
     switch (currentScreen) {
-
       case Screen.loading:
         return const LoadingScreen();
         break;
@@ -79,6 +79,7 @@ class Wrapper extends StatelessWidget {
       case Screen.resetPassword:
         return ResetPasswordScreen();
         break;
-    };
+    }
+    ;
   }
 }
