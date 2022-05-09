0b6533ca486c2b29a753c67d157e82b8f7d8e404
arturoszulc@gmail.com <MIlmx*Qx.ZK~Xt#KP.s&>
2022-05-09 19:53:43 +0200

    Started implementing InAppPurchases

---
+++ lib/model_layer/services/purchase_service.dart
@@ -0,0 +1,7 @@
+import 'package:in_app_purchase/in_app_purchase.dart';
+
+class PurchaseService {
+
+  final String _productID = "sepapka_pro";
+  final InAppPurchase _iap = InAppPurchase.instance;
+}
