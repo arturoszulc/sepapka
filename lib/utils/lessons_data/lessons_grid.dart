f8921b41418990ffc800f8e843730891fb2f7f0e
arturoszulc@gmail.com <MIlmx*Qx.ZK~Xt#KP.s&>
2022-07-05 08:37:23 +0200

    Corrected gridLessons

--- lib/utils/lessons_data/lessons_grid.dart
+++ lib/utils/lessons_data/lessons_grid.dart
@@ -14,23 +14,23 @@ const Lesson gridLesson1 = Lesson(
             'Oprócz przewodów fazowych (L), sieć może posiadać przewody: *neutralny (N)*, *ochronny (PE)* lub *ochronno-neutralny (PEN)*. '
             'W praktyce stosowane są sieci:'),
     LessonList(bullets: true, strings: [
-      'jednofazowe dwuprzewodowe (L+N)',
-      'jednofazowe dwuprzewodowe (L+PEN)',
-      'jednofazowe trójprzewodowe (L+N+PE)',
-      'trójfazowe trójprzewodowe (3\u00d7L)',
-      'trójfazowe czteroprzewodowe (3\u00d7L+N)',
-      'trójfazowe czteroprzewodowe (3\u00d7L+PE)',
-      'trójfazowe czteroprzewodowe (3\u00d7L+PEN)',
-      'trójfazowe pięcioprzewodowe (3\u00d7L+N+PE)',
+      'jednofazowe dwuprzewodowe (L+N),',
+      'jednofazowe dwuprzewodowe (L+PEN),',
+      'jednofazowe trójprzewodowe (L+N+PE),',
+      'trójfazowe trójprzewodowe (3\u00d7L),',
+      'trójfazowe czteroprzewodowe (3\u00d7L+N),',
+      'trójfazowe czteroprzewodowe (3\u00d7L+PE),',
+      'trójfazowe czteroprzewodowe (3\u00d7L+PEN),',
+      'trójfazowe pięcioprzewodowe (3\u00d7L+N+PE).',
     ]),
     LessonParagraph(
         text:
-            'To jakie przewody posiada sieć zależy od *sposobu jej uziemienia* oraz posiadania przewodu realizującego *funkcję ochronną*. O tym wszystkim mówi nam *typ sieci*.'),
+            'To jakie przewody posiada sieć zależy od *sposobu jej uziemienia* oraz rodzaju przewodu realizującego *funkcję ochronną*. O tym wszystkim mówi nam *typ sieci*.'),
     LessonHeader(title: 'Podstawowe typy sieci'),
     LessonParagraph(
         text:
             'Ze względu na sposób połączenia sieci z ziemią, wyróżniamy sieci typu *TN*, *TT* oraz *IT*. '
-            'Jeśli chodzi o przewód ochronny, to takowy posiada tylko sieć *TN* i w zależności od sposobu jego prowadzenia wyróżniamy podtypy *TN-S*, *TN-C* i *TN-C-S*.'),
+            'Jeśli chodzi o przewód ochronny, to takowy posiada tylko sieć *TN*. Stąd, ze względu na jego rodzaj, wyróżniamy podtypy *TN-S*, *TN-C* i *TN-C-S*.'),
     LessonCard(
         isImportant: true,
         text:
@@ -40,41 +40,43 @@ const Lesson gridLesson1 = Lesson(
     LessonParagraph(
         text: 'Symbole opisujące typ sieci wywodzą się z następujących francuskich określeń:'),
     LessonList(strings: [
-      'T – terre – ziemia',
-      'I – isolation – izolować',
-      'N – neutre – neutralny',
-      'S – separe  - oddzielny',
-      'C – combine – połączony',
+      'T – terre – ziemia,',
+      'I – isolation – izolować,',
+      'N – neutre – neutralny,',
+      'S – separe  - oddzielny,',
+      'C – combine – połączony.',
     ]),
     LessonHeader(title: 'Znaczenie symboli'),
     LessonParagraph(
         text:
             'Pierwsza litera typu sieci (np. w TN-S jest to litera T) opisuje sposób *połączenia sieci z ziemią*:'),
     LessonList(strings: [
-      'T - bezpośrednie (metaliczne) połączenie',
-      'I - całkowita izolacja lub połączenie przez dużą impedancję',
+      'T - bezpośrednie (metaliczne) połączenie,',
+      'I - całkowita izolacja lub połączenie przez dużą impedancję.',
     ]),
     LessonParagraph(
         text:
             'Druga litera opisuje sposób połączenia z ziemią *części przewodzących dostępnych*, czyli wszystkich metalowych elementów, które w razie awarii mogą znaleźć się pod napięciem:'),
     LessonList(strings: [
-      'T - bezpośrednie (metaliczne) połączenie',
-      'N - połączenie przez przewód ochronny wyprowadzony z uziemionego punktu sieci (najczęściej punktu neutralnego)',
+      'T - bezpośrednie (metaliczne) połączenie,',
+      'N - połączenie przez przewód ochronny wyprowadzony z uziemionego punktu sieci (najczęściej punktu neutralnego).',
     ]),
     LessonParagraph(
         text:
             'W przypadku sieci *TN*, po myślniku możemy znaleźć jeszcze dwa symbole, które określają typ *przewodu ochronnego*:'),
     LessonList(strings: [
-      'C - przewód ochronno-neutralny (PEN)',
-      'S - przewód ochronny (PE))',
-      'C-S - część sieci jak w układzie C, pozostała część jak w S',
+      'C - przewód ochronno-neutralny (PEN),',
+      'S - przewód ochronny (PE),',
+      'C-S - część sieci jak w układzie C, pozostała część jak w S.',
     ]),
     LessonCard(
         isExample: true,
         text:
-            'Sieć typu TN-C jest bezpośednio uziemiona (T) i wszystkie podłączone do niej części przewodzące dostępne są uziemione przez punkt neutralny (N). Posiada ona również przewód pełniący funkcję ochronną (C), w tym przypadku przewód ochronno-neutralny PEN.'),
+            'Sieć typu TN-C jest bezpośednio uziemiona (T), a wszystkie podłączone do niej części przewodzące dostępne są uziemione przez punkt neutralny (N). Posiada ona również przewód pełniący funkcję ochronną (C), w tym przypadku przewód ochronno-neutralny PEN.'),
   ],
 );
+
+
 
 const Lesson gridLesson2 = Lesson(
     id: 'SNP-2',
@@ -84,12 +86,12 @@ const Lesson gridLesson2 = Lesson(
   LessonHeader(title: 'Sieć TN - informacje ogólne'),
   LessonParagraph(text: 'Sieci typu TN znajdują zastosowanie jako:'),
   LessonList(bullets: true, strings: [
-    'sieci rozdzielcze zasilające budynki mieszkalne / użyteczności publicznej',
-    'instalacje elektryczne w budynkach i obiektach przemysłowych',
+    'sieci rozdzielcze zasilające budynki mieszkalne i użyteczności publicznej,',
+    'instalacje elektryczne w budynkach i obiektach przemysłowych.',
   ]),
   LessonParagraph(
       text: 'Sieć typu TN jest siecią *uziemioną* przynajmniej w jednym punkcie. '
-          'Z tego punktu wyprowadzony jest *przewód ochronny*, do którego podłączone są wszystkie *części przewodzące dostępne*.'),
+          'Z tego punktu wyprowadzony jest *przewód ochronny*, do którego podłączamy wszystkie *części przewodzące dostępne*.'),
   LessonParagraph(text: 'Ze względu na typ *przewodu ochronnego*, wyróżniamy sieci:'),
   LessonList(bullets: true, strings: [
     'TN-C - funkcję ochronną pełni przewód neutralny (PEN),',
@@ -100,8 +102,8 @@ const Lesson gridLesson2 = Lesson(
   LessonHeader(title: 'Sieć TN-C - budowa'),
   LessonParagraph(text: 'Sieć typu TN-C posiada następujące przewody:'),
   LessonList(bullets: true, strings: [
-    'fazowy (L)',
-    'ochronno-neutralny (PEN)',
+    'fazowy (L),',
+    'ochronno-neutralny (PEN).',
   ]),
   LessonParagraph(text:'W wersji jednofazowej jest to sieć *dwuprzewodowa*, zaś w trójfazowej *czteroprzewodowa*:'),
   ImageCaption(path: 'assets/images/grid/grid_tnc_1ph.png'),
@@ -110,9 +112,9 @@ const Lesson gridLesson2 = Lesson(
   LessonHeader(title: 'Sieć TN-S - budowa'),
   LessonParagraph(text: 'Sieć typu TN-S posiada następujące przewody:'),
   LessonList(bullets: true, strings: [
-    'fazowy (L)',
-    'neutralny (N)',
-    'ochronny (PE)',
+    'fazowy (L),',
+    'neutralny (N),',
+    'ochronny (PE).',
   ]),
   LessonParagraph(text: 'W wersji jednofazowej jest to sieć *trójprzewodowa*, zaś w trójfazowej *pięcioprzewodowa*:'),
   ImageCaption(path: 'assets/images/grid/grid_tns_1ph.png'),
@@ -121,8 +123,8 @@ const Lesson gridLesson2 = Lesson(
   LessonHeader(title: 'Sieć TN-C-S - budowa'),
   LessonParagraph(text: 'Sieć typu TN-C-S posiada następujące przewody:'),
   LessonList(bullets: true, strings: [
-    'fazowy (L)',
-    'ochronno-neutralny (PEN) (rozdzielony w pewnym momencie na PE i N)',
+    'fazowy (L),',
+    'ochronno-neutralny (PEN) (rozdzielony w pewnym momencie na PE i N).',
   ]),
   LessonParagraph(text: 'W wersji jednofazowej jest to sieć *dwuprzewodowa*, która w punkcie rozdziału zamienia się w *trójprzewodową*. Wersja trójfazowa zaś przechodzi z *czteroprzewodowej* w *pięcioprzewodową*.'),
   ImageCaption(path: 'assets/images/grid/grid_tncs_1ph.png'),
@@ -131,11 +133,11 @@ const Lesson gridLesson2 = Lesson(
 
 
   LessonHeader(title: 'Sieć TT - informacje ogólne'),
-  LessonParagraph(text: 'Sieci typu TT znajduje zastosowanie jako:'),
+  LessonParagraph(text: 'Sieci typu TT znajdują zastosowanie jako:'),
   LessonList(bullets: true, strings: [
-    'sieci rozdzielcze zasilające budynki mieszkalne / użyteczności publicznej',
-    'instalacje w budynkach i obiektach przemysłowych',
-    'instalacje w obiektach rolniczych i ogrodniczych',
+    'sieci rozdzielcze zasilające budynki mieszkalne i użyteczności publicznej,',
+    'instalacje w budynkach i obiektach przemysłowych,',
+    'instalacje w obiektach rolniczych i ogrodniczych.',
   ]),
   LessonParagraph(
       text: 'Sieć typu TT jest siecią *uziemioną* przynajmniej w jednym punkcie. '
@@ -153,16 +155,16 @@ const Lesson gridLesson2 = Lesson(
 
 
   LessonHeader(title: 'Sieć IT - informacje ogólne'),
-  LessonParagraph(text: 'Sieci typu IT stosuje się wszędzie tam, gdzie:'),
+  LessonParagraph(text: 'Sieć typu IT stosuje się wszędzie tam, gdzie:'),
   LessonList(bullets: true, strings: [
-    'utrzymanie ciągłości zasilania jest kluczowe',
-    'występuje zagrożenie pożarowe lub wybuchowe',
+    'utrzymanie ciągłości zasilania jest kluczowe,',
+    'występuje zagrożenie pożarowe lub wybuchowe.',
   ]),
   LessonParagraph(text: 'Stąd doskonale sprawdza się ona w roli:'),
   LessonList(bullets: true, strings: [
-    'instalacji w obiektach medycznych i serwerowniach',
-    'instalacji w przemyśle górniczym i chemicznym',
-    'instalacji oświetlenia bezpieczeństwa',
+    'instalacji w obiektach medycznych i serwerowniach,',
+    'instalacji w przemyśle górniczym i chemicznym,',
+    'instalacji oświetlenia bezpieczeństwa.',
   ]),
   LessonParagraph(
       text: 'Sieć typu IT jest siecią *nieuziemioną* lub *uziemioną przez dużą impedancję*. '
@@ -171,8 +173,8 @@ const Lesson gridLesson2 = Lesson(
   LessonHeader(title: 'Sieć IT - budowa'),
   LessonParagraph(text: 'Sieć typu IT posiada następujące przewody:'),
   LessonList(bullets: true, strings: [
-    'fazowy (L)',
-    'neutralny (N)',
+    'fazowy (L),',
+    'neutralny (N).',
   ]),
   LessonParagraph(text:'W wersji jednofazowej jest to sieć *dwuprzewodowa*, zaś w trójfazowej *czteroprzewodowa*.'),
   ImageCaption(path: 'assets/images/grid/grid_it_1ph.png'),
@@ -184,6 +186,8 @@ const Lesson gridLesson2 = Lesson(
 
 ]);
 
+
+
 const Lesson gridLesson3 = Lesson(
     id: '3',
     title: 'Zakresy napięciowe instalacji elektrycznych',
@@ -194,8 +198,8 @@ const Lesson gridLesson3 = Lesson(
       LessonList(
           bullets: true,
           strings: [
-            '1000 V dla prądu przemiennego',
-            '1500 V dla prądu stałego',
+            '1000 V dla prądu przemiennego,',
+            '1500 V dla prądu stałego.',
           ]),
       LessonParagraph(text: 'W przypadku prądu przemiennego mówimy o napięciu *skutecznym, międzyfazowym* o maksymalnej częstotliwości równej 60 Hz.'),
       LessonParagraph(text: 'Z kolei w przypadku prądu stałego mówimy tu o napięciu *między biegunami* źródła.'),
@@ -214,17 +218,18 @@ const Lesson gridLesson3 = Lesson(
       LessonList(
           bullets: true,
           strings: [
-            '600 V między fazą, a ziemią',
-            '1000 V między fazami',
+            '600 V między fazą, a ziemią,',
+            '1000 V między fazami.',
           ]),
+      LessonParagraph(text: 'Jeśli sieć jest nieuziemiona (typ IT), wówczas dotyczy jej limit 1000 V między fazami.'),
       LessonParagraph(text: 'Napięcie stałe *zakresu II* nie może przekroczyć:'),
       LessonList(
           bullets: true,
           strings: [
-            '900 V między fazą, a ziemią',
-            '1500 V między fazami',
+            '900 V między biegunem, a ziemią,',
+            '1500 V między biegunami.',
           ]),
-      LessonParagraph(text: 'Jeśli sieć jest nieuziemiona (typ IT), wówczas dotyczy jej limit 1500 V między fazami.'),
+      LessonParagraph(text: 'Jeśli sieć jest nieuziemiona (typ IT), wówczas dotyczy jej limit 1500 V między biegunami.'),
     ]);
 
 const Lesson gridLesson4 = Lesson(
