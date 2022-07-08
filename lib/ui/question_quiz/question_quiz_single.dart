2c84fead9a72b8fd302a3d8695e1da204d124234
arturoszulc@gmail.com <MIlmx*Qx.ZK~Xt#KP.s&>
2022-07-08 10:44:42 +0200

    Updated percent_indicator Fixed bug when swiping back in quiz didn't navigate to menu screen

--- lib/ui/question_quiz/question_quiz_single.dart
+++ lib/ui/question_quiz/question_quiz_single.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sepapka/controllers/quiz_controller.dart';
 import 'package:sepapka/models/button_map.dart';
 import 'package:sepapka/models/question.dart';
 import 'package:sepapka/utils/custom_widgets/dialog_leave_session.dart';
-import 'package:sepapka/controllers/quiz_controller.dart';
 
-import '../../utils/custom_widgets/buttons/answer_button.dart';
+import '../../utils/consts/my_screens.dart';
 import '../../utils/custom_widgets/build_question.dart';
+import '../../utils/custom_widgets/buttons/answer_button.dart';
 import '../../utils/custom_widgets/single_question_top_bar.dart';
 
 class QuizSingleQuestion extends ConsumerWidget {
@@ -23,45 +25,44 @@ class QuizSingleQuestion extends ConsumerWidget {
 
     return WillPopScope(
       onWillPop: () async {
-      bool result = await leaveSessionDialog(context);
-      if (result) {
-        // myManager.interruptSession();
+        bool result = await leaveSessionDialog(context);
+        if (result) {
+          context.goNamed(MyScreen.menu.name);
+        }
         return false;
-      }
-        return false;
-        // context.read<Manager>().navigate(Screen.menu);
       },
-    child: SafeArea(
-      // maintainBottomViewPadding: true,
-
+      child: SafeArea(
         //question single screen is made of:
         // - top bar with progress indicator and other things
         // - main area with questions and aswers
         child: Scaffold(
           body: Column(mainAxisSize: MainAxisSize.min, children: [
-                //build topBar
-                  singleQuestionTopBar(context),
-                  const SizedBox(height: 10.0,),
-                  Expanded(
-                      flex: 7,
-                      child: buildQuestion(context, question)),
-                  Expanded(
-                    flex: 8,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: bMapList.map((e) =>
-                        AnswerButton(
-                            answer: e.answer,
-                            color: e.color,
-                            onSelected: () {
-                              ref.read(quizController).checkAnswer(e.answer);
-                            })
-                        ).toList(),
-                    ),
-                  ),
-            Expanded(flex: 2, child: Container(),),
-                ]),
+            //build topBar
+            singleQuestionTopBar(context),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Expanded(flex: 7, child: buildQuestion(context, question)),
+            Expanded(
+              flex: 8,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: bMapList
+                    .map((e) => AnswerButton(
+                        answer: e.answer,
+                        color: e.color,
+                        onSelected: () {
+                          ref.read(quizController).checkAnswer(e.answer);
+                        }))
+                    .toList(),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(),
+            ),
+          ]),
           floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
           floatingActionButton: Visibility(
             visible: isAnswered ? true : false,
